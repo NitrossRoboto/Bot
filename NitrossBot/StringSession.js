72692f9680ba3b4b9ce682f4301dc991b5441bc1
Nitross Bots <92256139+NitrossRoboto@users.noreply.github.com>
2021-10-19 08:25:19 +0530

    Update StringSession.js

--- NitrossBot/StringSession.js
+++ NitrossBot/StringSession.js
@@ -5,7 +5,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('NITROSS_SESSION' in process.env && string === undefined) {
+        if ('RAVANA_SESSION' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
