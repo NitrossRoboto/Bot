d85d2f3df2025898a0fe4151eb5a76951a06a017
NitrossRoboto <nitrossbot@yandex.com>
2021-10-21 16:09:44 +0530

    All Files Are Deleted !!!!!!!!!!

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM fusuf/whatsasena:latest
-
-RUN git clone https://github.com/NitrossRoboto/Bot /root/Bot
-WORKDIR /root/Bot/
-ENV TZ=Europe/Istanbul
-RUN npm install supervisor -g
-RUN yarn install --no-audit
-
-CMD ["node", "bot.js"]
--- plugins/delete.js
+++
@@ -1,9 +0,0 @@
-const Nitross = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
-const DEL_DESC = "Deletes The Replied Message"
-
-Nitross.addCommand({pattern: 'del', fromMe: true, desc: DEL_DESC}, (async (message, match) => {
-
-  await message.reply_message.delete();
-
-}));
